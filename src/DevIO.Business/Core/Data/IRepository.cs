1939921c2ebd0ffc655302b902cac51f00a24dfd
Amanda Sousa Bezerra <amanda.sousabez@gmail.com>
2021-11-30 14:45:13 -0300

    initial commit

---
+++ src/DevIO.Business/Core/Data/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using DevIO.Business.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Core.Data
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity  /*so pode ser persistido entidades */
+    {
+        Task Adicionar(TEntity entity);
+        Task<TEntity> ObterPorId(Guid id);
+        Task<List<Entity>> ObterTodos();
+        Task Atualizar(TEntity entity);
+        Task Remover(Guid id);
+        Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate);
+        Task<int> SaveChanges();
+    }
+}
---
+++ src/DevIO.Infra/Data/Context/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using DevIO.Business.Models.Fornecedores;
+using DevIO.Business.Models.Produtos;
+using DevIO.Infra.Data.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace DevIO.Infra.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("DefaultConnection")
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar")
+                .HasMaxLength(100));     /* dessa forma eu não precisaria me preocupar com o tamanho no mapeamento das entidades */
+
+            modelBuilder.Configurations.Add(new FornecedorConfig());
+            modelBuilder.Configurations.Add(new EnderecoConfig());
+            modelBuilder.Configurations.Add(new ProdutoConfig());
+        }
+    }
+}
